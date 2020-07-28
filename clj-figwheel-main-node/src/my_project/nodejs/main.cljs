630851753614c16102ec79a3baf9157a74cd82b4
Pavel Ponomarev <writepavel@gmail.com>
2020-07-28 23:11:32 +0200

    fig in deps.end made te same as in package.json

--- clj-figwheel-main-node/src/my_project/nodejs/main.cljs
+++ clj-figwheel-main-node/src/my_project/nodejs/main.cljs
@@ -1,4 +1,4 @@
-(ns ^:figwheel-hooks baflo.nodejs.main
+(ns ^:figwheel-hooks my-project.nodejs.main
   (:require [cljs.nodejs :as nodejs]))
 
 (nodejs/enable-util-print!)
@@ -6,12 +6,14 @@
 (def hello-message "Hello world!!!")
 
 (defn -main []
-    (println "hello"))
+    (println "hello 2"))
 
 (set! *main-cli-fn* -main) ;; this is required
 
 (defn ^:before-load my-before-reload-callback []
-  (println "BEFORE reload!!!"))
+  (println "BEFORE reload 4!!!"))
 
 (defn ^:after-load my-after-reload-callback []
   (println "AFTER reload!!!"))
+
+(println "STARTED!2")
